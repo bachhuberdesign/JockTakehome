7d51731ff259b55d011f9f9a1c3d3ea82d94c4b9
Eric Bachhuber <bachhuberdesign@gmail.com>
2021-07-03 12:58:49 -0500

    feat: Prep for sorting

--- src/screens/EventPlayers/index.tsx
+++ src/screens/EventPlayers/index.tsx
@@ -1,10 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { FlatList, ListRenderItemInfo, TextInput, View } from 'react-native';
+import { sortBy } from 'lodash';
 
 import { TradeablesEntity } from '../../types';
 import * as staticNbaEventData from '../../../assets/data/nba_event.json';
 import { useCallback } from 'react';
 import { TradeableCard } from '../../components/TradeableCard';
+
+enum TradeableSortType {
+  PointsProjected = 'pointsProjected',
+  PointsScored = 'pointsScored',
+  PriceEstimated = 'priceEstimated',
+}
 
 interface Props {}
 
@@ -21,6 +28,9 @@ const transformStaticDataToTradeablesArray = (): TradeableResponse => {
 export const EventPlayers: React.FC<Props> = props => {
   const [tradeables, setTradeables] = useState<TradeablesEntity[]>([]);
   const [searchText, setSearchText] = useState<string>('');
+  const [sortType, setSortType] = useState<TradeableSortType>(
+    TradeableSortType.PointsProjected
+  );
 
   const fetchPlayers = async () => {
     try {
@@ -45,6 +55,31 @@ export const EventPlayers: React.FC<Props> = props => {
     return <View style={{ marginTop: 8 }} />;
   }, []);
 
+  const sortedTradeables = useMemo(() => {
+    switch (sortType) {
+      case TradeableSortType.PointsProjected:
+        return sortBy(
+          tradeables,
+          (tradeable: TradeablesEntity) => tradeable.points.projected
+        );
+      case TradeableSortType.PointsScored:
+        return sortBy(
+          tradeables,
+          (tradeable: TradeablesEntity) => tradeable.points.scored
+        );
+      case TradeableSortType.PriceEstimated:
+        return sortBy(
+          tradeables,
+          (tradeable: TradeablesEntity) => tradeable.price.estimated
+        );
+      default:
+        return sortBy(
+          tradeables,
+          (tradeable: TradeablesEntity) => tradeable.points.projected
+        );
+    }
+  }, [tradeables, sortType]);
+
   const onSearchTextChanged = (text: string) => {
     setSearchText(text);
   };
@@ -61,7 +96,7 @@ export const EventPlayers: React.FC<Props> = props => {
         value={searchText}
       />
       <FlatList
-        data={tradeables}
+        data={sortedTradeables}
         keyExtractor={item => `${item.id}`}
         renderItem={renderTradeable}
         ItemSeparatorComponent={renderSeparator}
